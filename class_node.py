495e6af9907a3b8dc8cd8088f6767985ba71c101
LeonovEvgenii <leonovevgenii2015@yandex.ru>
2022-05-17 20:39:10 +0500

    add function delete node

--- class_node.py
+++ class_node.py
@@ -3,10 +3,15 @@ class Node():
 
 	def __init__(self, name):
 		self.name = name
-		self.links = set()
+		self.links = list() # не нужно делать множестовм ссылки. Массивом будет проще удалять и сложнее объединять
+		self.revert_links = list()
 
 	def create_link(self, node):
-		self.links.add(node.name)
+		self.links.append(node)
+		node.revert_links.append(self)
+
+	def create_revert_link(self, node):
+		node.revert_links.append(self)
 
 	def __repr__(self):
 		return repr(self.name)
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def write_to_graph(list_nodes):
 			f.write("  \"" + node.name + "\";\n")
 			if len(node.links) > 0:
 				for link in node.links:
-					f.write("  \"" + node.name + "\" -> \"" + link + "\";\n")
+					f.write("  \"" + node.name + "\" -> \"" + link.name + "\";\n")
 		f.write("}\n")
 
 def clear_graph():
@@ -73,6 +73,58 @@ def convert_words_to_nodes(input_list_words):
 	return input_list_nodes
 
 
+def add_node_to_graph(input_list_nodes, graph_list_nodes):
+	
+	for node_input in input_list_nodes:
+
+		if len(graph_list_nodes) > 0:
+
+			number_compare_node = None
+
+			for i, node_graph in enumerate(graph_list_nodes):
+				if node_input == node_graph:
+					number_compare_node = i
+					break
+
+			if number_compare_node == None:
+				graph_list_nodes.append(node_input)
+			else:
+				for link_input in node_input.links:
+					if link_input not in graph_list_nodes[number_compare_node].links:
+						graph_list_nodes[number_compare_node].links.append(link_input)
+						link_input.create_revert_link(graph_list_nodes[number_compare_node])
+
+			# если нужно ссылку добавить, то и в новом элементе обратную ссылку делаем
+			# в функции обратной ссылки можно и прямую делать и абзац выше убать с ручным сравнением
+
+			# позже проверть как удалятся элементы, которые друг на друга ссылаются, должен остаться один, который не удаляется	
+		
+		else:
+			graph_list_nodes.append(node_input)
+		
+	return graph_list_nodes
+
+
+def delete_node_to_graph(input_list_nodes, graph_list_nodes):
+
+	for node_input in input_list_nodes:
+
+		if len(graph_list_nodes) > 0:
+
+			for node_graph in graph_list_nodes:
+				if node_input == node_graph:
+
+					for revert_link in node_graph.revert_links:
+						revert_link.links.remove(node_input)
+
+					graph_list_nodes.remove(node_graph)
+					break
+
+		else:
+			break
+		
+	return graph_list_nodes
+
 if __name__ == "__main__":
 
 	clear_graph()
@@ -87,31 +139,11 @@ if __name__ == "__main__":
 
 		input_list_nodes = convert_words_to_nodes(input_list_words)
 
-		for node_input in input_list_nodes:
-
-			if len(graph_list_nodes) > 0:
-
-				compare_node = None
-
-				for node_graph in graph_list_nodes:
-					if node_input == node_graph:
-						compare_node = node_graph
-						break
-
-				if compare_node == None:
-					graph_list_nodes.append(node_input)
-				else:
-					compare_node.links = node_input.links | compare_node.links
-
-			else:
-				graph_list_nodes.append(node_input)
-
+		graph_list_nodes = add_node_to_graph(input_list_nodes, graph_list_nodes)
+		# graph_list_nodes = delete_node_to_graph(input_list_nodes, graph_list_nodes)
 
 		write_to_graph(graph_list_nodes)
 
-	# _4 = Node("4")
-	# _1.create_link(_2)
-	# write_to_graph([_1, _2, _3, _4])
 
 
 

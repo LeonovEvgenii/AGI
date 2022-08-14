4b979655b315f6f5677094e6f5b785effc3f1a05
LeonovEvgenii <leonovevgenii2015@yandex.ru>
2022-08-14 16:46:46 +0500

    develop defenition

--- main.py
+++ main.py
@@ -4,189 +4,9 @@ import os
 import subprocess
 import json
 
-
-def write_to_graph(list_nodes, file_name):
-	with open(file_name, 'w') as f:
-		f.write("digraph G {\n")
-		for node in list_nodes:
-
-			f.write(node.view_to_graph())
-
-			# f.write("  \"" + node.name + "\";\n")
-			# if len(node.links) > 0:
-			# 	for link in node.links:
-			# 		f.write("  \"" + node.name + "\" -> \"" + link.name + "\";\n")
-		f.write("}\n")
-
-def clear_graph(file_name):
-	with open(file_name, 'w') as f:
-		f.write("digraph G {\n\n")
-		f.write("}\n")
-
-def get_obj_graph(file_name):
-	with open(file_name, 'r') as f:
-		lines = f.readlines()
-
-		elements = set()
-		links = set()
-		elements_link = set()
-		for line in lines[1:-1]:
-			if "shape" in line:
-				pass
-			if "->" in line:
-
-
-				result = re.findall(r'"(.*)" -> {(.*)}', line)
-
-				node = Node(result[0])
-				
-				
-
-
-				print(result)
-				print(line)
-
-
-		exit(0)
-
-	# 		l = re.sub(" |;|\n|\"", "", line)
-	# 		if l == '':
-	# 			continue
-	# 		l = l.split("->")
-	# 		if len(l) > 1:
-	# 			links.add((l[0], l[1]))
-	# 			elements_link.add(l[0])
-	# 			elements_link.add(l[1])
-			
-	# 		for _ in l:
-	# 			elements.add(_)
-
-	# graph_list_nodes = []
-	# for element in elements:
-	# 	graph_list_nodes.append(Node(element))
-
-	# # добавляю ссылки в список нод
-	# for link in links:
-	# 	for node in graph_list_nodes:
-	# 		if node.name == link[0]:
-	# 			for node_2 in graph_list_nodes:
-	# 				if node_2.name == link[1]:
-	# 					node.create_link(node_2)
-
-	# if len(graph_list_nodes) == 0:
-	# 	print("Граф был пустым")
-
-	# return graph_list_nodes
-
-def convert_words_to_nodes(input_list_words):
-	input_list_nodes = []
-
-	if len(input_list_words) == 1:
-		input_list_nodes.append(Node(input_list_words[0]))
-	else:
-		for i, word in enumerate(input_list_words):
-			node = Node(word)
-		
-			try:
-				node_2 = Node(input_list_words[i+1])
-				node.create_link(node_2)
-				input_list_nodes.append(node)
-			except IndexError:
-				input_list_nodes.append(node)
-		
-	return input_list_nodes
-
-
-def add_node_to_graph(input_list_nodes, graph_list_nodes):
-	
-	for node_input in input_list_nodes:
-
-		if len(graph_list_nodes) > 0:
-
-			number_compare_node = None
-
-			for i, node_graph in enumerate(graph_list_nodes):
-				if node_input == node_graph:
-					number_compare_node = i
-					break
-
-			if number_compare_node == None:
-				graph_list_nodes.append(node_input)
-			else:
-				for link_input in node_input.links:
-					if link_input not in graph_list_nodes[number_compare_node].links:
-						graph_list_nodes[number_compare_node].links.append(link_input)
-						link_input.create_revert_link(graph_list_nodes[number_compare_node])
-		
-		else:
-			graph_list_nodes.append(node_input)
-		
-	return graph_list_nodes
-
-
-def delete_node_to_graph(input_list_nodes, graph_list_nodes):
-
-	for node_input in input_list_nodes:
-
-		if len(graph_list_nodes) > 0:
-
-			for node_graph in graph_list_nodes:
-				if node_input == node_graph:
-
-					for revert_link in node_graph.revert_links:
-						revert_link.links.remove(node_input)
-
-					graph_list_nodes.remove(node_graph)
-					break
-
-		else:
-			break
-		
-	return graph_list_nodes
-
-def read_node_to_graph(input_list_nodes, graph_list_nodes, i = 0):
-	if i > 5:
-		return
-	
-	print(i)
-	input_node = input_list_nodes[0]
-	
-	for node_graph in graph_list_nodes:
-		if input_node == node_graph:
-			print(node_graph.links)
-
-			for link in node_graph.links:
-				i += 1
-				read_node_to_graph([link], graph_list_nodes, i)
-
-			# print(node_graph.revert_links)
-
-def run_programm(node_name):
-	path = os.getcwd() + "/python_programm"
-	files = os.listdir(path)
-	for file in files:
-		if node_name == file:
-			output = subprocess.check_output(["python3", path + "/" + file])
-			output = str(output)[2:-3]
-			return output
-
-def run_node(input_list_nodes, graph_list_nodes):
-	for graph_node in graph_list_nodes:
-		if input_list_nodes[0] == graph_node:
-			output = run_programm(graph_node.revert_links[0].name)
-			print(output)
-			
-
-if __name__ == "__main__":
-
-	# file_name = "graphs/local_graph.dot"
-	# clear_graph(file_name)
-
-	# file_name = "graphs/global_graph.dot"
+def get_input_words():
 
 	while 1:
-
-		# graph_list_nodes = get_obj_graph(file_name)
 		input_str = input("Ввод: ")
 		if input_str == "":
 			print("Введена пустая строка")
@@ -194,16 +14,72 @@ if __name__ == "__main__":
 
 		input_str = input_str.lower()
 
-		punctuation = '!"#$%&\'()*,-/;<=>@[\\]^_`{|}~'
+		punctuation = '!"#$%&\'()*,-/;<=>@[\\]^`{|}~'
 		for p in punctuation:
 			if p in input_str:
 				input_str = input_str.replace(p, '')
 
-		# if input_str[-1] == "?":
-		# 	question_mode = True
-		# 	input_str = input_str[:-1]
+		input_list_words = input_str.split(" ")
+		
+		input_list_words = [ i for i in input_list_words if i]
 
-		# избаиться от забинденых ключевых слов, сделать их тоже понятиями
+		if input_list_words:
+			return input_list_words
+		else:
+			print("Строка не содержит ни одного ключевого слова")
+
+
+def run_nodes(input_list_words):
+	
+	path_json = os.getcwd() + "/json"
+	json_files = os.listdir(path_json)
+
+	path_python = os.getcwd() + "/python_programm"
+
+	for i, word in enumerate(input_list_words):
+
+		for file in json_files:
+			if word == file[:-5]:
+				with open("json/" + file) as json_file:
+					data = json.load(json_file)
+
+					list_without_run_word = input_list_words.copy()
+					list_without_run_word.remove(word)
+
+					output = subprocess.check_output(["python3", path_python + "/" + data["file"]] + list_without_run_word, encoding='utf-8')
+
+					print(output)
+
+
+
+
+					# output_string = str(output)
+
+					# output_list = output_string.split(r'\r\n')
+
+					# for e in output_list:
+					# 	print(e)
+
+					# print(output.decode('utf-8'))
+
+
+	# сохранение всех параметров строки
+	# открытие и запуск программы с параметрами
+
+
+
+if __name__ == "__main__":
+
+	while 1:
+
+		input_list_words = get_input_words()
+
+		run_nodes(input_list_words)
+
+		exit(0)
+
+		
+
 		if "создай новое определение первого рода: " in input_str:
 			# definition_mode = True
 			input_str = input_str[39:] # 39, пототому что команда константой задана
---
+++ python_programm/save_node.py
@@ -0,0 +1,11 @@
+import sys
+import json
+
+for word in sys.argv[1:]:
+    defenition = {}
+    defenition['name'] = word
+    # defenition['file'] = 
+
+    with open("json/" + word + ".json", 'w') as outfile:
+        json.dump(defenition, outfile, ensure_ascii=False)
+

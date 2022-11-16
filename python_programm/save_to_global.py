92d4336ac6d476ed8ca72ac5369357624c2692fd
LeonovEvgenii <leonovevgenii2015@yandex.ru>
2022-11-16 20:54:07 +0500

    Начал думать над сохранением в глобальный граф

---
+++ python_programm/save_to_global.py
@@ -0,0 +1,17 @@
+import os
+import json
+
+
+# скопировать все json в папку с глобальными json
+# local_graph.json скопировать в global_graph.json
+
+# проверять если слова нет, то добавлять
+# если слово есть, то доплнить поля (сделаю но не сразу, а когда будет уточнение определений)
+
+# если есть связь со служебным словом, то не добавлять ее
+# xdot глобальный перересовать
+
+
+
+# with open(os.getcwd() + "/graphs/local_graph.json") as json_file:
+#     save_pairs = json.load(json_file)

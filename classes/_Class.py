bffdf1fcd71d944f2f1d582410fa796529dea8ee
LeonovEvgenii <leonovevgenii2015@yandex.ru>
2023-04-02 18:51:56 +0500

    Решения о работе с локальными

--- classes/_Class.py
+++ classes/_Class.py
@@ -6,8 +6,24 @@ class _Class():
         self.name = word
         self.list_objects = []
 
+        # self.create_empty_local_file_class()
+
+
+    def __str__(self):
+        return self.name
+    
+    # метод перевода из локального в глобальный, со всеми проверками
+    def local_to_global(self):
+        pass
+
+    def create_empty_local_file_class(self):
+
         # создание файла класса в локальном графе (в любом случае)
         # при смене БД, записи изменяются здесь
+        # по идее, в локальный граф можно вообще не писать ничего в фалы или БД,
+        # т к при перезапуске он все равно чистится.
+        # Нужен только метод перевода в глобальный.
+        
         path_to_main = os.path.abspath(os.curdir)
         path_to_local_clasess = path_to_main + "/database_json/local/"
         path_to_file_class = path_to_local_clasess + self.name + ".json"
@@ -20,10 +36,3 @@ class _Class():
             json.dump(json_dic, outfile, ensure_ascii=False)
         outfile.close()
 
-
-    def __str__(self):
-        return self.name
-    
-    # метод перевода из локального в глобальный, со всеми проверками
-    def local_to_global(self):
-        pass
--- classes/_Object.py
+++ classes/_Object.py
@@ -8,16 +8,14 @@ class _Object():
     def __init__(self, new_class, number_in_sentence):
         self.class_name = new_class.name
 
-        # добавить в список объектов в файл класса (локальный)
-
-
-
         self.number_in_sentence = number_in_sentence
 
         self.create_unix_time = time()
         readable_time_and_zone = datetime.utcfromtimestamp(self.create_unix_time) + timedelta(hours = 5)
         self.short_readable_time = readable_time_and_zone.strftime('%H:%M:%S')
 
+        # Пока не добавляю в список объектов в файл класса (локальный).
+        # Общеснено, почему объеснено в классе _Class
 
     def __str__(self):
         return self.class_name + "\t" + self.short_readable_time + "\t" + str(self.number_in_sentence)

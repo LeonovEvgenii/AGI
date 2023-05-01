861bfa5b22ea0dbb52fb4c82f78fc0a4d5a242fe
LeonovEvgenii <leonovevgenii2015@yandex.ru>
2023-05-01 14:30:09 +0500

    Изменил структуру папок

---
+++ knowledge_base/python_programm/palka.py
@@ -0,0 +1,10 @@
+import sys
+import json
+import os
+
+# sys.path.append(os.getcwd())
+# from util.functions import write_to_local_graph_json, print_to_xdot_local
+
+print("выполнился_код_палки")
+
+# print_to_xdot_local()

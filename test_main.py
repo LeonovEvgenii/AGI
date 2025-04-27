4f94e2858812d76f83975ee5aafb219ada1a733f
LeonovEvgenii <leonovevgenii2015@yandex.ru>
2025-04-27 09:20:48 +0500

    flake8

--- test_main.py
+++ test_main.py
@@ -1,3 +1,5 @@
+"""unit тесты проекта."""
+
 # есть еще библиотека pytest, но я пока не осознал ее необходимость
 # необходимо доставлять в отличии от unittest
 # pip install pytest
@@ -9,60 +11,61 @@ import unittest
 
 from scripts.classes.Console import Console
 from scripts.classes.drawers.Xdot import Xdot
-from scripts.classes.Core import Core
 
 
 class Main_test(unittest.TestCase):
+    """Основной класс для работы тестов."""
 
-    def test_console(self):
+    def __test_console(self):
 
         # print("\nТест модуля консоль.")
-        # print("На вход подается текст предложения, преобразуется в граф и обратно из графа в текст.")
+        # print("На вход подается текст предложения, преобразуется в граф и
+        # обратно из графа в текст.")
 
         console = Console()
 
-        input_graphs = console.content_to_graphs("1 2 3 4. 5 6 7 8.")
+        input_graphs = console.content_to_graphs('1 2 3 4. 5 6 7 8.')
 
         # for graph in input_graphs:
         #     print(graph)
 
         output_text = console.graphs_to_content(input_graphs)
 
-        self.assertEqual(output_text, "1 2 3 4. 5 6 7 8.")
+        self.assertEqual(output_text, '1 2 3 4. 5 6 7 8.')
 
-    def test_draw(self):
+    def __test_draw(self):
 
         # print("\nТест модуля рисователь.")
         # print("На вход подаются графы, рисуются, с различными опциями.")
 
         console = Console()
 
-        input_graphs = console.content_to_graphs("1 2 3 4. 5 6 7 8.")
+        input_graphs = console.content_to_graphs('1 2 3 4. 5 6 7 8.')
 
         xdot = Xdot()
 
         # далее требуется рисование именно нескольких графов
         xdot.draw(input_graphs)
 
-    def test_first_born(self):
+    def __test_first_born(self):
         pass
 
-    def test_second_born(self):
+    def __test_second_born(self):
         pass
 
-    def test_set_definition(self):
+    def __test_set_definition(self):
         pass
 
-    def test_get_definition(self):
+    def __test_get_definition(self):
         pass
 
-    def test_compare_definition(self):
+    def __test_compare_definition(self):
         pass
 
-    def test_one_dialog(self):
+    def __test_one_dialog(self):
         pass
 
-    def test_numeric(self):
+    def __test_numeric(self):
         pass
 
     # def test_one_sum(self):
@@ -77,7 +80,8 @@ class Main_test(unittest.TestCase):
 
     # 	output_graph = core.run_nodes_2(input_graph)
 
-    # 	# console.graph_to_content(output_graph, print=True) # если печатать в консоль (не для теста)
+    #   # если печатать в консоль (не для теста)
+    # 	# console.graph_to_content(output_graph, print=True)
     # 	output_text = console.graph_to_content(output_graph)
 
     # 	self.assertEqual(output_text, "5")
@@ -91,11 +95,11 @@ class Main_test(unittest.TestCase):
     # 	self.assertEqual(c, [5, 4])
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     mt = Main_test()
 
-    # вызывать методы по отдельности нужно, только если запуск осуществляется через голый python
-    # без указания модуля unittest (python3 test_main.py).
-    # При запуске через unittest "python -m unittest test_main.py" код вызова отдельных методов
-    # не учитывается.
-    mt.test_console()
+    # вызывать методы по отдельности нужно, только если запуск осуществляется
+    # через голый python без указания модуля unittest (python3 test_main.py).
+    # При запуске через unittest "python -m unittest test_main.py" код вызова
+    # отдельных методов не учитывается.
+    mt.__test_console()

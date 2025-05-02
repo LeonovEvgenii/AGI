dc84a1c3fc4e8407ea75d1d9b19cf53111445f4e
LeonovEvgenii <leonovevgenii2015@yandex.ru>
2025-05-02 09:05:16 +0500

    flake8

--- scripts/classes/graph/node/Node.py
+++ scripts/classes/graph/node/Node.py
@@ -1,12 +1,19 @@
-# будут ли бругие типы нод, например датчик?
+"""Файл вершины графа."""
+
+# будут ли другие типы нод, например датчик?
+
 
 class Node():
-    
+    """."""
+
     def __init__(self, name):
+        """."""
         self.name = name
 
     def return_name_for_draw(self):
+        """."""
         pass
 
     def __str__(self):
-        return "нода: " + self.name
+        """Используется при печати графа."""
+        return 'нода: ' + self.name

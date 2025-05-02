477c0fea10e96b3846704e23c8e3e242cef4a8c5
LeonovEvgenii <leonovevgenii2015@yandex.ru>
2025-05-02 08:24:17 +0500

    flake8

--- scripts/classes/First_born.py
+++ scripts/classes/First_born.py
@@ -1,11 +1,20 @@
+"""Файл первородной ноды.
+
+Первородная нода возвращает результат выполнения на python.
+"""
+
 from scripts.classes._Class import _Class
 
+
 class First_born(_Class):
+    """."""
 
     def __init__(self, word):
+        """."""
         super().__init__(word)
 
-        self.path_python_file = ""
+        self.path_python_file = ''
 
     def __str__(self) -> str:
-        return "первородная: " + self.name
+        """Используется при печати графа."""
+        return 'первородная: ' + self.name
